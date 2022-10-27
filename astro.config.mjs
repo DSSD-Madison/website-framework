ef176ee27c00b9dbfab1f86f0c7d2022933579f2
Jack Blake <jhblake2@wisc.edu>
2022-10-27 09:54:20 -0500

    setup tailwind integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,11 @@ import { defineConfig } from 'astro/config';
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  integrations: [tailwind()]
 });
