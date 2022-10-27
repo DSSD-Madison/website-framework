f45641c5c5580915b97d96464c337cf33728e920
Jack Blake <jhblake2@wisc.edu>
2022-10-27 11:55:11 -0500

    setup tailwind theme and remove Astro template

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,17 @@
+const colors = require('tailwindcss/colors')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		colors: { ...colors,
+			blue: { ...colors.blue,
+				400: '#0091E9',
+				500: '#006FB2',
+				900: '#0D1C26'
+			}
+		}
 	},
 	plugins: [],
 }
