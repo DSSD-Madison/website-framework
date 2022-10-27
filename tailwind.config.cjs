da172a064aa3a222bdcc446b958b479a4d0a1532
Jack Blake <jhblake2@wisc.edu>
2022-10-27 11:57:58 -0500

    add labels to tailwind custom colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,9 +7,9 @@ module.exports = {
 		extend: {},
 		colors: { ...colors,
 			blue: { ...colors.blue,
-				400: '#0091E9',
-				500: '#006FB2',
-				900: '#0D1C26'
+				400: '#0091E9', //dssd logo's blue, mainly for buttons and accents
+				500: '#006FB2', //slightly darkened dssd logo blue, good for text on light background
+				900: '#0D1C26' //very dark blue for text or background
 			}
 		}
 	},
