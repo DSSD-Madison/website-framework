1b39fca6d9d818357dfdc8cf397a24e830f01ff0
Jack Blake <jhblake2@wisc.edu>
2022-11-08 17:14:59 -0600

    switch to Netlify for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import cloudflare from "@astrojs/cloudflare";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: cloudflare(),
+  adapter: netlify(),
   integrations: [tailwind()]
 });
