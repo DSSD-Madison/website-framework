5122cf72610b0b660b9bd0d31c96b64f67043cac
Sarthak Agrawal <sarthavader@gmail.com>
2022-11-09 23:48:53 -0600

    temp save

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,7 @@ const colors = require('tailwindcss/colors')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', "./node_modules/flowbite/**/*.js"],
 	theme: {
 		extend: {},
 		colors: { ...colors,
@@ -13,5 +13,7 @@ module.exports = {
 			}
 		}
 	},
-	plugins: [],
+	plugins: [
+		require('flowbite/plugin')
+	],
 }
