1501e5fd0829c8cb718fb6fb65df8d5526f3ce1d
Eric Wan <ericwan808@gmail.com>
2022-11-15 16:01:13 -0600

    created a skeleton for the people page and the Person component

---
+++ src/services/contentful.js
@@ -0,0 +1,17 @@
+import contentful from 'contentful'
+
+const client = contentful.createClient({
+  space: import.meta.env.CONTENTFUL_SPACE,
+  environment: import.meta.env.CONTENTFUL_ENVIRONMENT,
+  accessToken: import.meta.env.CONTENTFUL_ACCESS_TOKEN
+})
+
+//For exploring out content model
+export async function getContentTypes() {
+  return await client.getContentTypes()
+}
+
+export async function getAbout(){
+    let entries = await client.getEntries({ content_type: 'about' });
+    return entries.items[0].fields;
+}
