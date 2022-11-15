1d379b873d66f2c95c51a065755eb41f055bece3
Eric Wan <ericwan808@gmail.com>
2022-11-15 16:01:13 -0600

    created a skeleton for the people page and the Person component

--- src/services/contentful.js
+++ src/services/contentful.js
@@ -1,9 +1,15 @@
 import contentful from 'contentful'
 
-const client = contentful.createClient({
+/*const client = contentful.createClient({
   space: import.meta.env.CONTENTFUL_SPACE,
   environment: import.meta.env.CONTENTFUL_ENVIRONMENT,
   accessToken: import.meta.env.CONTENTFUL_ACCESS_TOKEN
+})*/
+
+const client = contentful.createClient({
+  space: "x00i2tlytr27",
+  environment: "master",
+  accessToken: "YtMYObOqRHyvWmeAap9CRSG7a46tFA8AOkE_ENTpygs"
 })
 
 //For exploring out content model
